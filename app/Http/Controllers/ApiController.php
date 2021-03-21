c82759a21f91acf1f5b8ccc7b068833021373df1
190109389 <78698917+190109389@users.noreply.github.com>
2021-03-21 14:33:32 +0600

    apicontroller added

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    //return posts
+    public function index(Request $request){
+        $posts = Post::all();
+        return response($posts, 200);
+    }
+
+    //return post with required id
+    public function get_post(Request $request){
+        $post = Post::find($request->$post_id);
+        if($post == null){
+        return response('post is not found');
+        }
+        return response($post, 200);
+    }
+}
